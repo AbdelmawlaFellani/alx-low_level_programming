92993658f04d590e95d4cd7e8056272ef81afcc0
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-03-21 20:38:23 +0000

    1st Commit: Task 0._putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
-
+#include <string.h>
+#include "main.h"
 /**
  * main - Entry point
- *
- * Retrun: Always 0 (Success)
+ * Description: 'Prints the String _putchar using the putchar prototype'
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-        printf("_putchar\n");
-        return (0);
+	char str[] = "rahctup_";
+	int l = strlen(str);
+
+	while (l > 0)
+	{
+		_putchar(str[l - 1]);
+		l--;
+	}
+	_putchar('\n');
+	return (0);
 }
