04e7825ebf2341ab81cb58a51c65ea5986e17826
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-03-22 04:41:47 +0000

    13th Commit: Task 12.The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,12 +2,37 @@
 #include <stdio.h>
 
 /**
+ * Step1 - is a condition made it cuz betty didnt stop complaining
+ *
+ * @i: same as i in our print_times_table
+ *
+ * @j: same as j in our print_times_table
+ *
+ * @val: same as val in our print_times_table
+ *
+ * Return: void
+ */
+void Step1(int i, int j, int val)
+{
+	{
+		_putchar(44);
+		if (val / 100 == 0 || val >= 99)
+		{
+			_putchar(32);
+			if ((i * (j + 1)) / 100 == 0)
+				_putchar(32);
+		}
+	}
+}
+
+/**
  * print_times_table - prints the @param times table, starting with 0
  *
  * @n: the parametre to decide which times table we want
  *
  * Return: void
  */
+
 void print_times_table(int n)
 {
 	int i = 0, val, j;
@@ -34,17 +59,7 @@ void print_times_table(int n)
 					_putchar(val + '0');
 				}
 				if (j < n)
-				{
-					_putchar(44);
-					if (val / 100 == 0 || val >= 99)
-					{
-						_putchar(32);
-						if ((i * (j + 1)) / 100 == 0)
-						{
-							_putchar(32);
-						}
-					}
-				}
+					Step1(i, j, val);
 				j++;
 			}
 			i++;
