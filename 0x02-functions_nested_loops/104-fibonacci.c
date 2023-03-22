5ab9d705c8fe6870c709a9b47d325f117259ae80
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-03-22 22:08:42 +0000

    17th Commit: Testing#2

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,17 +12,13 @@ int main(void)
 	unsigned long int t1 = 1, t2 = 2, n;
 	int i;
 
-	printf("%ld, %ld, ", t1, t2);
+	printf("%lu, %lu", t1, t2);
 	for (i = 3; i <= 98; ++i)
 	{
 		n = t1 + t2;
 		t1 = t2;
 		t2 = n;
-		printf("%ld", n);
-		if (i != 98)
-		{
-			printf(", ");
-		}
+		printf(", %lu", n);
 	}
 	printf("\n");
 	return (0);
