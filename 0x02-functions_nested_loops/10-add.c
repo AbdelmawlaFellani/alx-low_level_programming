9d5ece46d8b32e9c7028aa240c593e5e75d36cdb
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-03-22 01:29:08 +0000

    11th Commit: Task 10.a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ *
+ * @n: the first parametre that we gonna add to the second.
+ *
+ * @m: the second parametre that we gonna add to the first.
+ *
+ * Return: the Value of the Sum of those two params
+ */
+int add(int n, int m)
+{
+	return (n + m);
+}
