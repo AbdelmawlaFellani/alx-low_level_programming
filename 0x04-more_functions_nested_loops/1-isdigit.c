e2fd3f9266161846faba38301359421c75046537
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-03-23 22:54:01 +0000

    2nd Commit: Task 1.isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - Entry point
+ *
+ * @c : Input to check
+ *
+ * Return : 1 if input is digit and 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if ( c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
