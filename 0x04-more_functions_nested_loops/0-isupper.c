95ade27bf604ec1d6bbfef42fc933b56b163b3c8
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-03-23 22:21:37 +0000

    1st Commit: Task 0.isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,11 +3,13 @@
 /**
  * _isupper - checks for uppercase character
  *
+ * @c : the Value we want to check
+ * 
  * Return: 1 if uppercase and 0 otherwise
  */
 int _isupper(int c)
 {
-	if (c >= 65 && c <=90)
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
