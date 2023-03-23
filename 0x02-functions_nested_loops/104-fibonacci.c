08ff4a5ebca2e328c601f7b41be161a31009ea0d
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-03-23 00:36:09 +0000

    17th Commit: Testing#22

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define MAX 10000000000
 
 /**
  * main - Entry point
@@ -9,16 +10,34 @@
  */
 int main(void)
 {
-	unsigned long int t1 = 1, t2 = 2, n;
+	unsigned long t1 = 1, t2 = 2, ne1 = 0, ne2 = 0;
+	unsigned long pack1, pack2, pack3;
 	int i;
 
 	printf("%lu, %lu", t1, t2);
 	for (i = 3; i <= 98; ++i)
 	{
-		n = t1 + t2;
-		t1 = t2;
-		t2 = n;
-		printf(", %lu", n);
+		if (t1 + t2 > MAX || ne2 > 0 || ne1 > 0)
+		{
+			pack1 = (t1 + t2) / MAX;
+			pack2 = (t1 + t2) % MAX;
+			pack3 = ne2 + ne1 + pack1;
+
+			ne1 = ne2;
+			ne2 = pack3;
+
+			t1 = t2;
+			t2 = pack2;
+
+			printf(", %lu%010lu", ne2, t2);
+		}
+		else
+		{
+			pack2 = t1 + t2;
+			t1 = t2;
+			t2 = pack2;
+			printf(", %lu", t2);
+		}
 	}
 	printf("\n");
 	return (0);
