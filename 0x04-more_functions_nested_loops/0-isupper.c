604fc214dae9ddc52f0dece33ef5614c4b7eadb2
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-03-23 19:20:07 +0000

    1st Commit: Task 0.isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ *
+ * Return: 1 if uppercase and 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <=90)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
