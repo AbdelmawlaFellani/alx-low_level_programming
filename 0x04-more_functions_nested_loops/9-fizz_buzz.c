d87c6d181e331b71b86e2508fd994a8ec4378782
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-03-24 01:32:39 +0000

    10th Commit: Task 9.Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Fizz-Buzz test
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if ( i % 3 == 0 || i % 5 == 0)
+		{
+			if (i % 3 == 0)
+				printf("Fizz");
+			if (i % 5 == 0)
+				printf("Buzz");
+		}
+		else
+			printf("%d", i);
+
+		if (i != 100)
+			printf(" ");
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
