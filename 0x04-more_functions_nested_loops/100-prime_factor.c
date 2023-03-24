5e779f18bebb202481f832dc99657d92aeedc9d0
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-03-24 02:48:48 +0000

    12th Commit: Task 11.The problem of distinguishing prime numbers from composite numbers and of resolving the latter into their prime factors is known to be one of the most important and useful in arithmetic

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * last_factor - Gets the last prime Factor of a number
+ *
+ * @n: the Input we want to get his prime factors
+ *
+ * Return: the value of the last prime factor of a number
+ */
+int last_factor(long int n) {
+
+    int lastf = 0, i;
+
+    while (n % 2 == 0) {
+        lastf = 2;
+        n = n / 2;
+    }
+    for (i = 3; i*i <= n; i += 2) {
+        while (n % i == 0) {
+            lastf = i;
+            n = n / i;
+        }
+    }
+    if (n > 2) {
+        lastf = n;
+    }
+    return lastf;
+}
+
+
+/**
+ * main - Entry point
+ *
+ * Description: a program that prints the highest prime factor of a number
+ *
+ * Return : Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%d\n", last_factor(612852475143));
+
+	return (0);	
+}
