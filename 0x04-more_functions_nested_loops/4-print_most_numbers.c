05614db891e455a35ca4c5b1af2e7a9b74ecd10e
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-03-24 00:04:29 +0000

    5th Commit: Task 4. I believe in numbers and signs

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers from 0 to 9
+ * without including 2 and 4
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		if (i != '2' && i != '4')
+			_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
