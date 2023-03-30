d5bda11581354eb6106e9c96b78144d35fa10b27
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-03-30 19:31:47 +0000

    7th Commit: Task 6.Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * cap_string -  capitalizes all words of a string. 
+ *
+ * @s: pointer to a string
+ *
+ * Return: pointer to a all cap string
+ */
+char *cap_string(char *s)
+{
+	char *str = s;
+
+	while (*str)
+	{
+		if (*str >= 'a' && *str <= 'z')
+			if (*(str - 1) == '\n' ||*(str - 1) == '\t' 
+					||*(str - 1) == ' ' || *(str - 1) == '.')
+				*str -= 32;
+		str++;
+	}
+	return (s);
+}
