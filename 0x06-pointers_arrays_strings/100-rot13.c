aa90f3fcf1351a4d709d82e53fd71da0307c1ae8
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-03-30 22:23:47 +0000

    9th Commit: Task 8.rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * rot13 - 
+ *
+ * @s: pointer to a string
+ *
+ * Return: a new crypted string with the rot13
+ */
+char *rot13(char *s)
+{
+	char *str = s, *pa, *pr;
+	char *a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *r = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (*str)
+	{
+		pa = a;
+		pr = r;
+		while (*pa)
+		{
+			if(*str == *pa)
+			{
+				*str = *pr;
+				break;
+			}	
+			pa++;
+			pr++;
+		}
+		str++;
+	}
+	
+
+	return (s);
+}
