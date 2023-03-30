3ec2467cb95583aa8b87e26be9fe4ee6f79cec71
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-03-30 23:00:35 +0000

    10th Commit: Task 9.Numbers have life; they're not just symbols on paper

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_number - 
+ *
+ * @n: number
+ *
+ * Return: (int) printing the number @n
+ */
+void print_number(int n)
+{
+	unsigned int val;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		val = n * (-1);
+
+	}
+	else
+		val = n;
+
+	if (val / 10)
+	{
+		print_number(val / 10);
+	}
+	_putchar(val % 10 + '0');
+
+}
