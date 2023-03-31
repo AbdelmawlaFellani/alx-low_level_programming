b39d9b1afb421948924f6b5e7647d3008e3f8dd0
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-03-31 03:18:36 +0000

    Damn this task is a Crazy good practice

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,67 @@
+#include "main.h"
+
+
+/**
+ * infinite_add - adds two numbers.
+ *
+ * @n1: the first number
+ * @n2: the second number
+ * @r: the buffer that the function will use to store the result
+ * @size_r: the buffer size
+ *
+ * Return: pointer to the result
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	char *p1 = n1, *p2 = n2, *pr = r;
+	int carry, sum;
+	char tmp;
+
+	while (*p1)
+	{
+		p1++;
+	}
+	while (*p2)
+	{
+		p2++;
+	}
+
+	carry = 0;
+	p1--;
+	p2--;
+	while (p1 >= n1 || p2 >= n2 || carry > 0 || pr - r < size_r - 1)
+	{
+		sum = carry;
+		if (p1 >= n1)
+		{
+			sum += (*p1 - '0');
+			p1--;
+		}
+		if (p2 >= n2)
+		{
+			sum += (*p2 - '0');
+			p2--;
+		}
+		*pr = (sum % 10) + '0';
+		pr++;
+		carry = sum / 10;
+	}	
+	*pr = '\0';
+
+	p1 = r;
+	p2 = pr - 1;
+
+	while (p1 < p2)
+	{
+		tmp = *p1;
+		*p1 = *p2;
+		*p2 = tmp;
+		p1++;
+		p2--;
+	}
+	if (pr - r > size_r - 1)
+	{
+		return (0);
+	}
+	return (r);
+}
