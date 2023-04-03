ba7fdcdb52ce96df4e0284a9cba042c9f91f1460
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-04-03 21:19:28 +0000

    3rd Commit: Task 2.strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr -  locates a character in a string.
+ *
+ * @s : the String input
+ * @c : the character input
+ *
+ * Return: pointer to the first occurrence of the character in the string
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			break;
+		s++;
+	}
+	return (s);
+}
