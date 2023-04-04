00961d08918eb163252350083743348229f02688
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-04-04 01:42:34 +0000

    8th Commit: Task 7.Chess is mental torture

--- 0x07-pointers_arrays_strings/5-main.c
+++ 0x07-pointers_arrays_strings/5-main.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	char *s = "hello, world asdasdasdasdasasd";
+	char *s = "hello, world";
 	char *f = "world";
 	char *t;
 
---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard.
+ *
+ * @a : array input
+ *
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i = 0, j = 0;
+
+	while (i < 8)
+	{
+		j = 0;
+		while (j < 8)
+		{
+			_putchar(a[i][j]);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
