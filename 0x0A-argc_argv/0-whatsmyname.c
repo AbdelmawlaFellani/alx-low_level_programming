727cdba8f409133d18f832230978e15fd741da66
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-04-09 13:36:44 +0000

    1st Commit: Task 0.my name is

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * Main - Entry Point
+ *
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: arg counter value
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n",argv[0]);
+	return (argc);
+}
