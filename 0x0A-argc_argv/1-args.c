c18eb28cee57a60f019e686cabaef773f3298236
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-04-10 06:18:17 +0000

    2nd Commit: Task 1.Argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry Point
+ *
+ * @argc: arguments counter
+ * @argv: arguments vector
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n",argc - 1);
+	return (0);
+}
