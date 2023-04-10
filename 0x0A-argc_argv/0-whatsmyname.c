af90828f2f28baa644ad9d0512a102c8dcf800f9
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-04-10 06:13:45 +0000

    1st Commit: Task 0.mynameis

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,10 +6,10 @@
  * @argc: argument counter
  * @argv: argument vector
  *
- * Return: arg counter value
+ * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((unused)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
-	return (argc);
+	return (0);
 }
