10cf56146a37899196b3c47f9b4d3f39af287967
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-04-12 12:27:17 +0000

    7th Commit: Task 6. I will show you how great I am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,6 +1,52 @@
 #include "main.h"
 
 /**
+ * total_length - counts total length of each word in a given string
+ * @str: the string to count from
+ * Return: the total length
+ */
+int total_length(char *str)
+{
+	int total_len = 0, word_len = 0, i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ')
+		{
+			total_len += word_len;
+			word_len = 0;
+		}
+		else
+			word_len++;
+	}
+	total_len += word_len;
+
+	return (total_len);
+}
+
+/**
+ * count_words - counts words in a given string
+ * @str: the string to count words from
+ * Return: the number of words
+ */
+
+int count_words(char *str)
+{
+	int wc = 0;
+	int i = 0;
+
+	while (*(str + i))
+	{
+		if (*(str + i) != ' ' && (*(str + i + 1) == ' '
+			|| *(str + i + 1) == '\0'))
+			wc++;
+		i++;
+
+	}
+	return (wc);
+}
+
+/**
  * strtow - splits a string into words.
  *
  * @str: String input
@@ -10,25 +56,16 @@
  */
 char **strtow(char *str)
 {
-	int i, j, len = 0, wc = 0;
-	char *newStr;
-	char **words;
+	int i = 0, j = 0, len = 0, wc = 0;
+	char *newStr, **words;
 
 	if (str == NULL || *str == '\0')
 		return (NULL);
-	i = 0;
-	while (*(str + len))
-	{
-		if (*(str + len) != ' ' && (*(str + len + 1) == ' ' || *(str + len + 1) == '\0'))
-			wc++;
-		len++;
-
-	}
+	wc = count_words(str);
+	len = total_length(str);
 	newStr = malloc(sizeof(char) * (len + wc));
 	if (newStr == NULL)
 		return (NULL);
-	i = 0;
-	j = 0;
 	while (*(str + i) != '\0')
 	{
 		if (*(str + i) != ' ')
@@ -44,13 +81,9 @@ char **strtow(char *str)
 		i++;
 	}
 	if (*(newStr + j - 1) == '\n')
-	{
 		*(newStr + j - 1) = '\0';
-	}
 	else
-	{
 		*(newStr + j) = '\0';
-	}
 
 	words = malloc(sizeof(char *));
 	if (words == NULL)
