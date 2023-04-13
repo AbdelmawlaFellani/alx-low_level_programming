c3719674426c8dd35a4a664b2efc1aa1cedc4793
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-04-13 10:13:20 +0000

    1st Commit: Task 0. Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc.
+ *
+ * @b: is the size of the memory we want to allocate
+ *
+ * Return: pointer to the allocated memory, 98 in case of faillure
+ */
+void *malloc_checked(unsigned int b)
+{	
+	void *p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return p;
+}
