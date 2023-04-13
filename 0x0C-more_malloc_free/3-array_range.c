640049f850081aa1a938a46dbfa770dd0c161eb7
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-04-13 17:39:50 +0000

    4th Commit: Task 3.array_range

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * _memset - fills the first @n bytes of the memory area with @c
+ *
+ * @s: pointer to the starting memory location where the value will be set
+ * @c: value to be set
+ * @n: number of bytes to be set to the value
+ *
+ * Return: pointer to the memory block
+ */
+void *_memset(void *s, int c, unsigned int n)
+{
+	unsigned int len = 0;
+	int *ps = s;
+
+	while (len < n)
+	{
+		*ps++ = c++;
+		len += 4;
+	}
+	return (s);
+}
+/**
+ * array_range -  creates an array of integers.
+ *
+ * @min: minimum integer value provided
+ * @max: maximum integer value provided
+ *
+ * Return: pointer to an array of integers starting from @min to @max
+ */
+int *array_range(int min, int max)
+{
+	int *p;
+	
+
+	if (min > max)
+		return (NULL);
+
+	p = malloc(sizeof(int) * (max - min + 1));
+
+	if (!p)
+		return (NULL);
+
+	_memset(p, min, sizeof(int) * ((max - min) + 1));
+
+	return (p);
+}
