bf99ec433eb4cf7603a9b489161703ab40d6c4ca
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-04-14 21:46:29 +0000

    Setup Last Task

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: multi of two numbers from agruments
+ */
+int main(int argc,char *argv[])
+{
+	return (0);
+}
