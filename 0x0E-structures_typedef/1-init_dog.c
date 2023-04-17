d84f617b0b0bada11d4934eaa05e2caac79bf86e
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-04-17 21:10:28 +0000

    2nd Commit: TASK 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * init_dog -  initialize a variable of type struct dog
+ *
+ * @d: struct dog
+ * @name: name Input
+ * @age: age Input
+ * @owner: owner Input
+ *
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
