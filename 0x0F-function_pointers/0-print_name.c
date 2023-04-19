b04f6832fc47eebc2036487e0fa4904bd6f8486b
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-04-19 18:56:17 +0000

    1st Commit: TASK 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ *
+ * @name: string input
+ * @f: callback function 
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
