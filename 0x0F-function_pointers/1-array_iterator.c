3687d11de4aa619bd5e2f7659a73b1195d3f079a
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-04-19 20:05:01 +0000

    2nd Commit: TASK 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,7 +14,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (!array || !size)
+	if (!array || !size || !action)
 		return;
 
 	for (i = 0; i < size; i++)
