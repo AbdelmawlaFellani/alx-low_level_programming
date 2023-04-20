1a12ebeab973f0e3ab19dcab8e2db24f648e058a
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-04-20 14:29:12 +0000

    2nd Commit: TASK 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints Numbers
+ *
+ * @separator: a string to be printed between the args
+ * @n: is the Number of the args will be provided
+ *
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+
+	va_list args;
+
+	va_start(args, n);
+
+	while (i < n - 1)
+	{
+		printf("%d%s", va_arg(args, int), separator);
+		i++;
+	}
+
+	printf("%d\n", va_arg(args, int));
+
+	va_end(args);
+
+}
