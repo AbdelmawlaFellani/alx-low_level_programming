03e8b6dc12249f6095d1b783f299e083d98764df
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-04-28 01:28:47 +0000

    6th Commit: TASK 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
+/**
+ * first - prints A whole String before main function
+ *
+ * Return: void
+ */
 void __attribute__((constructor)) first()
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n
+	I bore my house upon my back!\n");
 }
