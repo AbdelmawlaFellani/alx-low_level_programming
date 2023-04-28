53f30d2e6fad146cfaea6de453795a5cf3f4a60e
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-04-28 01:16:49 +0000

    5th Commit: TASK 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,12 +10,13 @@
  */
 void free_list(list_t *head)
 {
-	while (head->next != NULL)
+	list_t *next;
+
+	while (head != NULL)
 	{
+		next = head->next;
 		free(head->str);
 		free(head);
-		head = head->next;
+		head = next;
 	}
-	free(head->str);
-	free(head);
 }
