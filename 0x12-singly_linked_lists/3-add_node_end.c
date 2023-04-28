4d4c7502428fb84a94548e192afb7d7e417697d4
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-04-28 01:04:28 +0000

    4th Commit: TASK 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -2,6 +2,14 @@
 #include <string.h>
 #include <stdlib.h>
 
+/**
+ * add_node_end - adds a new node at the end of a list_t list.
+ *
+ * @head: pointer to the first node
+ * @str: the String Input we want to duplicate inside the linked list
+ *
+ * Return: the address of the new Element
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new;
