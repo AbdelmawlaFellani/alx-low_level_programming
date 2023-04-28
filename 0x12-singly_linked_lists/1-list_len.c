13ea51ae5da177fd5111475d4efe4dbb80346fd3
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-04-28 00:00:46 +0000

    2nd Commit: TASK 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,6 +1,13 @@
 #include "lists.h"
 #include <stdio.h>
 
+/**
+ * list_len - returns the number of elements in a linked list_t list.
+ *
+ * @h: singely linked list Input
+ *
+ * Return: number of elements in a linked list
+ */
 size_t list_len(const list_t *h)
 {
 	size_t elemCount = 0;
