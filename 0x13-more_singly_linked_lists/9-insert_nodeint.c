997c957679735c7feaa5352a8d52b8f29c3fedf1
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-05-03 00:52:17 +0100

    10th Commit: TASK 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,7 +32,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			if (prev)
 				prev->next = new;
 			else
-				curr = new;
+				*head = new;
 
 			new->next = curr;
 			return (new);
