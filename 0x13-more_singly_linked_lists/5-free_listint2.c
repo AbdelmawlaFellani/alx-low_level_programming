926ed9b3a45be61a2418d71154d3d2e3bc0a118a
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-05-02 21:57:53 +0100

    6th Commit: TASK 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * free_listint2 - frees a listint_t list.
+ *
+ * @head: pointer to the first node in the linked list
+ *
+ * Return: void
+ */
 void free_listint2(listint_t **head)
 {
 	listint_t *next;
