b3be5e08d2d429efc28d839938bc3e6ffd6eb4eb
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-05-03 01:09:06 +0100

    10th Commit: TASK 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,6 +13,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int index = 0;
 	listint_t *new, *curr, *prev;
+	
+	if (!head)
+		return (NULL);
 
 	new = malloc(sizeof(listint_t));
 
@@ -21,6 +24,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new->n = n;
 	new->next = NULL;
+	
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
 
 	prev = NULL;
 	curr = *head;
