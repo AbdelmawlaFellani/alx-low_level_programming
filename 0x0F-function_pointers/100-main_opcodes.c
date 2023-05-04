927827684ed7d91133a3b94e4785574737a397e5
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-05-04 02:19:29 +0100

    Testing OLD TASKS - TASK ADVANCED

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -11,6 +11,33 @@
  */
 int main(int ac, char *av[])
 {
-	(int)ac;
-	(char *)av;
+	int bytes, i;
+	char *arr;
+	/* Checks the case if the number of arguments is correct or not */
+	if (ac != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	
+	bytes = atoi(av[1]); /* Args are strings we convert them to integers */
+	/* Checks if the number of bytes is negative */
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	arr = (char *)main;
+
+	while (i < bytes)
+	{
+		if (i == (bytes - 1))
+		{
+			printf("%02hhx\n",arr[i]);
+			break;
+		}
+		printf("%02hhx\n",arr[i]);
+		i++;
+	}
+	return (0);
 }
