cc7a84765589781cd9d1cb4407e240f4e9945906
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-05-05 00:29:09 +0100

    2nd Commit: TASK 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask = 1;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	while (mask <= n)
+		mask <<= 1;
+
+	if (mask > n)
+		mask >>= 1;
+	
+	while (mask)
+	{
+		if (mask & n)
+			_putchar('1');
+		else
+			_putchar('0');
+		mask >>= 1;
+	}
+}
