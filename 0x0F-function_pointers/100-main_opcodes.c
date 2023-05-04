5e0ca04b77af40dafc83610125a15db24209b4ba
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-05-04 02:01:58 +0100

    Testing OLD TASKS - TASK ADVANCED

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry Point
+ *
+ * @ac: Argument Counter
+ * @av: Argument Vector
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int ac, char *av[])
+{
+	(int)ac;
+	(char *)av;
+}
