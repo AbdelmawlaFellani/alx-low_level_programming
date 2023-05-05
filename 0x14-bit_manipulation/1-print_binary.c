e30949c2a3b6753e2719f0e134bc79c3d9c15a62
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-05-05 01:03:30 +0100

    2nd Commit: TASK 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,30 +3,20 @@
 /**
  * print_binary - prints the binary representation of a number.
  *
- * @n: int input
+ * @n: unsigned long int input
  *
  * Return: void
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = 1;
-
-	if (n == 0)
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + 48);
+	}
+	else
 	{
 		_putchar('0');
-		return;
-	}
-
-	while (mask <= n)
-		mask <<= 1;
-	mask >>= 1;
-
-	while (mask)
-	{
-		if (mask & n)
-			_putchar('1');
-		else
-			_putchar('0');
-		mask >>= 1;
 	}
 }
