0e6d759a27853649b892f42f0d5ba68f9508af1e
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-05-11 16:32:50 +0100

    Advanced Task - ELF TASK

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Displays the information contained in the ELF
+ *
+ * @argc: Argument Counter
+ * @argv: Argument Vector
+ *
+ * Return: 0 on success.
+ */
+int main()
+{
+	return (0);
+}
