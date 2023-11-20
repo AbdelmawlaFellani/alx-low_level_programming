9fad13ec78e1e816242cf8cb0ebe816f4ff56946
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-11-21 00:49:47 +0100

    hash Tables Revision

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Gets an element of the hash table
+ * @ht: the hash table you want to look into
+ * @key: the key you are looking for
+ *
+ * Return: the value associated with the element
+ * or NULL if key not found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	uint index;
+	hash_node_t *curr_node;
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	curr_node = ht->array[index];
+
+	while (curr_node != NULL)
+	{
+		if (strcmp(curr_node->key, key) == 0)
+		{
+			return (curr_node->value);
+		}
+		curr_node = curr_node->next;
+	}
+	return (NULL);
+}
