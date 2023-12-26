0a74445c8f947e1e3ea46ea4f075ec4cde408308
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-12-26 17:38:15 +0100

    Task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,13 +12,17 @@ def island_perimeter(grid):
     for row in range(rows):
         for col in range(cols):
             if grid[row][col] == 1:
-                if row == 0 or grid[row - 1][col] == 0: #Checking UP
-                   perimeter += 1
-                if row == rows-1 or grid[row + 1][col] == 0: #Checking DOWN
+                # Checking UP
+                if row == 0 or grid[row - 1][col] == 0:
                     perimeter += 1
-                if col == 0 or grid[row][col - 1] == 0: #Checking LEFT
+                # Checking DOWN
+                if row == rows-1 or grid[row + 1][col] == 0:
                     perimeter += 1
-                if col == cols-1 or grid[row][col + 1] == 0: #Checking RIGHT
+                # Checking LEFT
+                if col == 0 or grid[row][col - 1] == 0:
+                    perimeter += 1
+                # Checking RIGHT
+                if col == cols-1 or grid[row][col + 1] == 0:
                     perimeter += 1
 
     return perimeter
