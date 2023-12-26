33e4c59d9e3acbdd79742761e205564c7d3e2de8
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-12-26 17:29:02 +0100

    5 island Perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """
+    Returns the perimeter of the island described in grid
+    """
+
+    perimetre = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                if row == 0 or grid[row - 1][col] == 0: #Checking UP
+                   perimeter += 1
+                if row == rows - 1 or grid[row + 1][col] == 0: #Checking DOWN
+                    perimeter += 1
+                if col == 0 or grid[row][col - 1] == 0: #Checking LEFT
+                    perimeter += 1
+                if col == cols - 1 or grid[row][col + 1] == 0: #Checking RIGHT
+                    perimeter += 1
+    return (perimetre)
